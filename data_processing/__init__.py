fa2342b79c9cf967cc7330d7067a1850e877e7cc
Neo-Nguyen-95 <dzung.usth@gmail.com>
2024-08-24 10:02:17 +0700

    complete extract finance data

--- data_processing/__init__.py
+++ data_processing/__init__.py
@@ -1,8 +1,1 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Aug 23 15:05:28 2024
-
-@author: dungnguyen
-"""
-
+from .cafef_scraping import FinanceStat
--- data_processing/cafef_scraping.py
+++ data_processing/cafef_scraping.py
@@ -1,13 +1,72 @@
+#%% IMPORT LIBRARY
 import pandas as pd
+import datetime
 
 pd.set_option('display.max_columns', None)
 
-# in the url, year number shown in the final column 
-url = 'https://s.cafef.vn/bao-cao-tai-chinh/fpt/cashflow/2023/0/0/0/0/luu-chuyen-tien-te-gian-tiep-.chn'
-df = pd.read_html(url)
+#%% CLASS
+class FinanceStat:
+    
+    # input
+    def __init__(self, company_name='fpt'):
+        self.company_name = company_name
+        # internal variable
+        self.year_1st = 2000
+        self.year_final = datetime.datetime.now().year
+    
+    def get_category(self, report_type):
+        url = ('https://s.cafef.vn/bao-cao-tai-chinh/' 
+               + self.company_name + '/' 
+               + report_type + '/' 
+               + '2024' + 
+               '/0/0/0/0/luu-chuyen-tien-te-gian-tiep-.chn'
+               )  # pick a random year to get category
+        df = pd.read_html(url)
+        df_temp = df[4]
+        df_category = df_temp.iloc[:, 0]
+        return df_category
+        
+    
+    def get_stat(self, report_type):
+        
+        year_list = range(self.year_1st, self.year_final+1)
+        
+        result = pd.DataFrame(columns = year_list)
+        
+        for year in year_list:
+            url = ('https://s.cafef.vn/bao-cao-tai-chinh/' 
+                    + self.company_name + '/' 
+                    + report_type + '/' 
+                    + str(year) + 
+                    '/0/0/0/0/luu-chuyen-tien-te-gian-tiep-.chn'
+                    )
+            
+            web_data = pd.read_html(url)
 
-# construct dataframe from data
-df_fin = df[4]
-df_fin.columns = df[3].iloc[0].values
-df_fin.drop(columns='Tăng trưởng', inplace=True)
-df_fin.head()
+            # construct dataframe from data         
+            result[year] = web_data[4].iloc[:, 4]
+            
+        
+            # check if data available or not
+            if result[year].isna().sum() == len(result):
+                result.drop(columns=year, inplace=True)
+
+        return result
+    
+    def get_findata(self, report_type):
+        return pd.concat([self.get_category(report_type),
+                          self.get_stat(report_type)],
+                         axis='columns')
+    
+    def export_findata(self, form = 'csv'):
+        # export all report data
+        for report_type in ['cashflow', 'incsta', 'bsheet']:
+            file_name = self.company_name + '_' + report_type
+            if form == 'csv':
+                self.get_findata(report_type).to_csv(file_name + '.csv')
+            else:
+                self.get_findata(report_type).to_excel(file_name + '.xlsx')
+        
+#%% TEST_SITE
+# model = FinanceStat()
+# print(model.get_findata('cashflow').head())
